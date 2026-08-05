2085a905e9da284effaba49a4671b1b31e6241b6
amirnasr94 <amir.nasr72@gmail.com>
2026-08-05 22:16:35 +0330

    implemented ThemeProvider to handle dark and light mode with ThemeToggle component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import { ThemeProvider } from "@/components/ThemProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,8 +24,19 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <main className="container">{children}</main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
