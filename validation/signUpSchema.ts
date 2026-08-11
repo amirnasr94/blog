9c3b8a69bcf3b01740fe3854240643ac120e907b
amirnasr94 <amir.nasr72@gmail.com>
2026-08-11 23:47:33 +0330

    implement signup validation schema

---
+++ validation/signUpSchema.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+
+export const signUpSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: "Name is required" })
+    .max(50, { message: "Name must be less than 50 characters" }),
+  email: z.email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" })
+    .max(30, { message: "Password must be less than 30 characters" }),
+});
