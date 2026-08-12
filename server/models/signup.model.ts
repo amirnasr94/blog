6097138599ab3b820da9796d3f7ffb875c55c1d1
amirnasr94 <amir.nasr72@gmail.com>
2026-08-12 16:18:34 +0330

    implement SignUp model to talk to mongo

---
+++ server/models/signup.model.ts
@@ -0,0 +1,43 @@
+import { Document, model, models, Schema } from "mongoose";
+
+export interface ISignup extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const SignupSchema = new Schema<ISignup>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+      maxlength: 30,
+    },
+  },
+  { collection: "users", timestamps: true },
+);
+
+const SignUpModel =
+  models.SignupModel || model<ISignup>("SignupModel", SignupSchema);
+
+export default SignUpModel;
