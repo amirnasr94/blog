2310dfd241ad41d652431c31978448125578a3fc
amirnasr94 <amir.nasr72@gmail.com>
2026-08-13 23:39:14 +0330

    use tree shaking

--- validation/signUpSchema.ts
+++ validation/signUpSchema.ts
@@ -1,13 +1,11 @@
-import z from "zod";
+import { object, string, email } from "zod";
 
-export const signUpSchema = z.object({
-  name: z
-    .string()
+export const signUpSchema = object({
+  name: string()
     .min(1, { message: "Name is required" })
     .max(50, { message: "Name must be less than 50 characters" }),
-  email: z.email({ message: "Invalid email address" }),
-  password: z
-    .string()
+  email: email({ message: "Invalid email address" }),
+  password: string()
     .min(6, { message: "Password must be at least 6 characters long" })
     .max(30, { message: "Password must be less than 30 characters" }),
 });
