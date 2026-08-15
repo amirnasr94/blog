c884179342faf7cfd258d36c88132b71a201f032
amirnasr94 <amir.nasr72@gmail.com>
2026-08-15 23:44:33 +0330

    implement Authentication
    encript and decript and create session and save in coockie based of user's email

---
+++ lib/session.ts
@@ -0,0 +1,70 @@
+import "server-only";
+import { SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+
+type Payload = {
+  userEmail: string;
+  expireDate: Date;
+};
+
+const secretKey = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export async function encript(payload: Payload) {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("7d")
+    .sign(encodedKey);
+}
+
+export async function decript(session: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (error) {
+    console.log("Failed To Verify Session!", error);
+  }
+}
+
+export async function createSession(userEmail: string) {
+  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  const session = await encript({
+    userEmail,
+    expireDate: expires,
+  });
+  const cookieStore = await cookies();
+
+  cookieStore.set("session", session, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+    path: "/",
+  });
+}
+
+export async function updateSession() {
+  const session = (await cookies()).get("session")?.value;
+  const payload = decript(session);
+  if (!session || !payload) {
+    return null;
+  }
+  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  const cookieStore = await cookies();
+  cookieStore.set("session", session, {
+    httpOnly: true,
+    secure: true,
+    expires: expires,
+    sameSite: "lax",
+    path: "/",
+  });
+}
+
+export async function deleteSession() {
+  const cookie = await cookies();
+  cookie.delete("session");
+}
