169e06d7697d648c2a939fbc92ecee5e3e43a471
amirnasr94 <amir.nasr72@gmail.com>
2026-08-21 17:34:13 +0330

    implement func to compare a value with hashed of value

--- lib/hashing.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export async function hashing(value: string) {
-  if (!value) return null;
-  const salt = await bcrypt.genSalt(10);
-  const hashed = await bcrypt.hash(value, salt);
-  return hashed;
-}
