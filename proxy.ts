76fa8ea0a63e48ad2de492ecc1f9fcf413e99423
amirnasr94 <amir.nasr72@gmail.com>
2026-08-21 17:29:40 +0330

    modify proxy based of verifySession func

--- proxy.ts
+++ proxy.ts
@@ -1,26 +1,23 @@
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import { decript } from "./lib/session";
+import { verifySession } from "./lib/session";
 
-const protectedRoutes = ["create-blog"];
-// const publicRoutes = ["sign-up", "login", "/"];
+const protectedRoutes = ["/create-blog"];
+// const publicRoutes = ["/sign-up", "/login", "/"];
 
 export default async function proxy(req: NextRequest) {
   const path = req.nextUrl.pathname;
+  console.log(req.nextUrl);
 
   const isProtectedRoute = protectedRoutes.includes(path);
   //   const isPublicRoute = publicRoutes.includes(path);
 
-  const cookie = (await cookies()).get("session")?.value;
-  const session = await decript(cookie);
+  const { isAuth } = await verifySession();
 
-  if (isProtectedRoute && !session?.useEmail) {
-    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  if (!isAuth && isProtectedRoute) {
+    return NextResponse.redirect(new URL("/login", req.url));
   }
-
   return NextResponse.next();
 }
-
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
 };
