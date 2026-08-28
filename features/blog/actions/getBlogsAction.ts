f9279e616637d3cfb625893e2b149472130a512a
amirnasr94 <amir.nasr72@gmail.com>
2026-08-28 15:07:18 +0330

    implement getBlogs action

---
+++ features/blog/actions/getBlogsAction.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { getUser } from "@/features/auth/actions/getUser";
+import connectDB from "@/server/config/mongoConfig";
+import Blog, { IBlog } from "@/server/models/blog.model";
+
+type Returned = Promise<
+  | {
+      data: IBlog[] | null;
+      status: number;
+    }
+  | undefined
+>;
+
+export default async function getBlogsAction(): Returned {
+  try {
+    await connectDB();
+    const userInfo = await getUser();
+
+    let author;
+
+    if (userInfo?.status === 200) {
+      author = userInfo.data?.email;
+    } else author = process.env.NEXT_PUBLIC_AUTHOR;
+
+    if (!author) {
+      return {
+        data: [],
+        status: 200,
+      };
+    }
+
+    const response = await Blog.find({
+      "author.email": author,
+    })
+      .populate("author", "name email")
+      .lean();
+
+    return {
+      data: response,
+      status: 200,
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        data: null,
+        status: 500,
+      };
+    }
+  }
+}
